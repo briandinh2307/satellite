d456f207e8d4f6b10b194382b52fbe638e67b98f
briandinh2307 <briandinh2307@gmail.com>
2019-12-28 03:57:19 +1030

    before distance tracking

--- constellation.cc
+++ constellation.cc
@@ -1,5 +1,4 @@
 #include "constellation.h"
-#include "ns3/geographic-positions.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/network-module.h"
 #include "ns3/netanim-module.h"
@@ -12,196 +11,41 @@
 
 using namespace ns3;
 
-struct SatCoord
-{
-    double satLong;
-    double satLati;
-    double satAlti;
-};
-
-class Constellation
-{
-    public:
-        Constellation(double c_alt, double c_inc, uint c_nPlane, uint c_nSat):
-                      alt(c_alt), inc(c_inc), nPlane(c_nPlane), nSat(c_nSat) {}
-
-        ~ Constellation()
-        {
-            delete[] polarSat;
-        }
-
-        void SetSatellite();
-        void Set(uint satNode);
-        Vector SphericalToCartesianCoordinates(double, double, double);
-        SatCoord SatPos(uint satNode);
-        Coordinate Coord();
-        
-    protected:
-        double alt;
-        double inc;
-        uint nPlane;
-        uint nSat;
-        double period;
-        Coordinate init;
-        SatCoord satellite;
-        OrbitalSat* polarSat = new OrbitalSat [nPlane * nSat];
-        SatGeometry geometry;
-};
-
-
-void Constellation::SetSatellite()
-{
-    double satAngle = 360.0/nSat;
-    double planeAngle = 360.0/nPlane;
-    double offsetMulti = 5.0;
-    double phaseOffset = offsetMulti/nPlane;
-    for(uint a = 0; a < nPlane; a++)
-        for(uint i = 0; i < nSat; i++)
-        {   
-            polarSat[a*nSat+i].altitude = alt;
-            polarSat[a*nSat+i].inclination =  inc;
-            polarSat[a*nSat+i].longitude = planeAngle * a;
-            polarSat[a*nSat+i].alpha = i*satAngle + a*phaseOffset*satAngle;
-            polarSat[a*nSat+i].plane = a;
-        }
-
-    std::cout << polarSat[25].alpha << std::endl;
-}
-
-void Constellation::Set(uint satNode)
-{
-    double satAlt = polarSat[satNode].altitude; 
-    double satLon = polarSat[satNode].longitude;
-    double satAlpha = polarSat[satNode].alpha;
-    double satIncl = polarSat[satNode].inclination;
-
-    // Check parameter
-    // std::cout << "Node: " << satNode << std::endl;
-    // std::cout << "alpha: " << satAlpha << std::endl;
-    // std::cout << "longitude: " << satLon << std::endl;
-    // std::cout << "inclination: " << satIncl << std::endl;
-    //
-	init.r = satAlt + EARTH_RADIUS; // Altitude in km above the earth
-	if (satAlpha < 0)
-        exit(1);
-    
-    if (satAlpha >= 360)
-        init.theta = DEG_TO_RAD(satAlpha - 360);
-    else
-    {
-        init.theta = DEG_TO_RAD(satAlpha);
-    }
-    // if (satLon < -180 || satLon > 180)
-	// 	exit(1);
-	if(satLon < 0)
-		init.phi = DEG_TO_RAD(360 + satLon);
-	else
-		init.phi = DEG_TO_RAD(satLon);
-
-    // Check parameter
-    // if(satNode == 25)
-    // {
-    //     std::cout << "theta: " << init.theta << std::endl;
-    // }
-    if(satIncl < 0 || satIncl > 180)
-		exit(1);
-	inc = DEG_TO_RAD(satIncl);
-
-	double num = init.r * init.r * init.r;
-	period = 2 * PI * sqrt(num/MU); // seconds
-}
-
-Vector Constellation::SphericalToCartesianCoordinates(double r, double theta, double phi)
-{
-    double x = r * sin(theta) * cos (phi);
-	double y = r * sin(theta) * sin (phi);
-	double z = r * cos(theta);
-    Vector cartesian(x, y, z);
-    return cartesian;
-}
-
-Coordinate Constellation::Coord()
-{
-	Coordinate current;
-	double partial;  // fraction of orbit period completed
-	partial = (fmod(Simulator::Now().GetSeconds(), period)/period) * 2*PI; //rad
-	double theta_cur, phi_cur, theta_new, phi_new;
-
-	// Compute current orbit-centric coordinates:
-	// theta_cur adds effects of time (orbital rotation) to init.theta
-	theta_cur = fmod(init.theta + partial, 2*PI);
-	phi_cur = init.phi;
-	// Reminder:  theta_cur and phi_cur are temporal translations of 
-	// initial parameters and are NOT true spherical coordinates.
-	//
-	// Now generate actual spherical coordinates,
-	// with 0 < theta_new < PI and 0 < phi_new < 360
-
-	//assert (inc < PI);
-
-	// asin returns value between -PI/2 and PI/2, so 
-	// theta_new guaranteed to be between 0 and PI
-	theta_new = PI/2 - asin(sin(inc) * sin(theta_cur));
-	// if theta_new is between PI/2 and 3*PI/2, must correct
-	// for return value of atan()
-	if ((theta_cur > PI/2 && theta_cur < 3*PI/2) || theta_cur == DEG_TO_RAD(270.0))
-		phi_new = atan(cos(inc) * tan(theta_cur)) + phi_cur + PI;
-	else
-		phi_new = atan(cos(inc) * tan(theta_cur)) + phi_cur;
-	phi_new = fmod(phi_new + 2*PI, 2*PI);
-	
-	current.r = init.r;
-	current.theta = theta_new;
-	current.phi = phi_new;
-	return current;
-}
-
-SatCoord Constellation::SatPos(uint sat)
-{
-    Set(sat);
-    satellite.satLati = RAD_TO_DEG(geometry.get_latitude(Coord()));
-    satellite.satLong = RAD_TO_DEG(geometry.get_longitude(Coord()));
-    satellite.satAlti = geometry.get_altitude(Coord());
-
-    return satellite;
-    //return SphericalToCartesianCoordinates(init.r, init.theta, init.phi);
-}
-
 static void 
-PositionCall (Ptr<const Node> node, uint tmp_sat)
+PositionCall (Ptr<const Node> node, uint32_t tmp_sat)
 {
     Ptr<MobilityModel> tmp_pos = node->GetObject<MobilityModel>();
-    uint sat_th = node->GetId();
-    uint plane_th = sat_th / tmp_sat;
+    uint32_t sat_th = node->GetId();
+    uint32_t plane_th = sat_th / tmp_sat;
 
     Vector pos = tmp_pos->GetPosition();
     std::cout << "t = " << Simulator::Now().GetSeconds() << "s" << ", " << sat_th + 1 << ", "
               << plane_th + 1 << ", " << pos.x << ", " << pos.y << ", " << pos.z << std::endl;
 }
 
-void Initialize(NodeContainer* iSat, Constellation* iTeledesic)
+void SetSatPos (NodeContainer* satPos, Constellation* wDelta)
 {
-    SatCoord geoPos;
-    for (NodeContainer::Iterator iter = iSat->Begin(); iter != iSat->End(); ++iter)
+    Vector geoPos;
+    for (NodeContainer::Iterator iter = satPos->Begin(); iter != satPos->End(); ++iter)
     {
         Ptr<Node> tmp_node = (*iter);
-        uint sat = tmp_node->GetId();
-        geoPos = iTeledesic->SatPos(sat);
+        uint32_t sat = tmp_node->GetId();
+        geoPos = wDelta->SatPos(sat);
 
-        double latitude = geoPos.satLati;
-        double longitude = geoPos.satLong;
-        double altitude = geoPos.satAlti;
+        double longitude = geoPos.x;
+        double latitude = geoPos.y;
+        double altitude = geoPos.z;
 
-        iSat->Get(sat)->GetObject<MobilityModel>()->SetPosition(Vector(longitude, latitude, altitude));
+        satPos->Get(sat)->GetObject<MobilityModel>()->SetPosition(Vector(longitude, latitude, altitude));
     }
-    Simulator::Schedule (Seconds (0.2), &Initialize, iSat, iTeledesic);
+    Simulator::Schedule (Seconds (0.2), &SetSatPos, satPos, wDelta);
 }
 
-double distance(Vector a, Vector b)
+// Check for position
+static void RetrievePos(Constellation* netWork)
 {
-    double dist_x = (a.x - b.x)*(a.x - b.x);
-    double dist_y = (a.x - b.x)*(a.x - b.x);
-    return sqrt(dist_x + dist_y);
+    Vector* a = netWork->GetPos();
+    std::cout << "x: " << a[1].x << " y: " << a[1].y << std::endl;
 }
 
 int main (int argc, char *argv[])
@@ -209,8 +53,8 @@ int main (int argc, char *argv[])
 
     double alt = teledesic_alt;
     double inc = teledesic_inc;
-    uint nPlane = teledesic_nPlane;
-    uint nSat = teledesic_nSat;
+    uint32_t nPlane = teledesic_nPlane;
+    uint32_t nSat = teledesic_nSat;
     std::string animFile = "constellation.xml";
     double watchTime = 0.0;
     double simTime = 100.0;
@@ -235,21 +79,19 @@ int main (int argc, char *argv[])
     satellite.Create (nPlane * nSat);
 
     MobilityHelper mobility;
-
-    Constellation satNetWork(alt, inc, nPlane, nSat);
-    satNetWork.SetSatellite();
-    
-    //mobility.SetPositionAllocator ("ns3::UniformDiscPositionAllocator");
     mobility.Install (satellite);
 
-    Initialize(&satellite, &satNetWork);
+    Constellation satNetWork(alt, inc, nPlane, nSat);
+    SetSatPos(&satellite, &satNetWork);
 
+    // Check for position
+    Simulator::Schedule (Seconds(watchTime), &RetrievePos, &satNetWork);
     // std::vector<SatCoord> link;
-    // for(uint a = 0; a < nPlane; a++)
-    //     for(uint i = 0; i < nSat; i++)
+    // for(uint32_t a = 0; a < nPlane; a++)
+    //     for(uint32_t i = 0; i < nSat; i++)
     //     {
-    //         uint cur = a*nSat+i;
-    //         uint next = (a+1)*nSat+i;
+    //         uint32_t cur = a*nSat+i;
+    //         uint32_t next = (a+1)*nSat+i;
     //         Ptr<MobilityModel> cur_sat = satellite.Get(cur)->GetObject<MobilityModel>();
     //         Vector cur_pos = cur_sat->GetPosition();
     //         Ptr<MobilityModel> next_sat = satellite.Get(next)->GetObject<MobilityModel>();
@@ -258,10 +100,10 @@ int main (int argc, char *argv[])
             
 
     PointToPointHelper pointToPoint;
-    for(uint a = 0; a < nPlane; a++)
-        for(uint i = 0; i < nSat; i++)
+    for(uint32_t a = 0; a < nPlane; a++)
+        for(uint32_t i = 0; i < nSat; i++)
         {
-            uint link = a*nSat + i;
+            uint32_t link = a*nSat + i;
             // Intraplane link
             if(i == nSat - 1)
                 pointToPoint.Install(satellite.Get(link), satellite.Get(a*nSat));
@@ -295,8 +137,8 @@ int main (int argc, char *argv[])
     AnimationInterface anim(animFile);
     anim.SetBackgroundImage("1.gif", -180.0, -90.0, 0.75, 0.75, 0.5);
 
-    for(uint a = 0; a < nPlane; a++)
-            for(uint i = 0; i < nSat; i++)
+    for(uint32_t a = 0; a < nPlane; a++)
+            for(uint32_t i = 0; i < nSat; i++)
             {
                 //Plane 1
                 if(a == 0)
--- constellation.h
+++ constellation.h
@@ -26,28 +26,19 @@ struct Coordinate
         // double z = r * cos(theta);
 };
 
-struct OrbitalSat
-{
-        double altitude;
-        double inclination;
-        double longitude;
-        double alpha;
-        double plane;
-};  
-
 // Library of routines involving satellite geometry
 class SatGeometry
 {
 public:
-	SatGeometry(){}
-	// static double distance(coordinate, coordinate);              
+	SatGeometry (){}
+	double distance (Vector a, Vector b);              
 	// static void spherical_to_cartesian(double, double, double,
 	//     double &, double &, double &);
 	// static double propdelay(coordinate, coordinate);
-	double get_latitude(Coordinate);
-	double get_longitude(Coordinate);
-	double get_radius(Coordinate a) { return a.r; }
-	double get_altitude(Coordinate);
+	double get_latitude (Coordinate);
+	double get_longitude (Coordinate);
+	double get_radius (Coordinate a) { return a.r; }
+	double get_altitude (Coordinate);
 	// static double check_elevation(Coordinate, Coordinate, double);
 	// static int are_satellites_mutually_visible(Coordinate, Coordinate);
 
@@ -56,13 +47,20 @@ protected:
         int command(/*int argc, const char*const* argv */) { return 0; }
 };
 
-double SatGeometry::get_altitude(Coordinate a)
+double SatGeometry::distance (Vector a, Vector b)
+{
+    double dist_x = (a.x - b.x) * (a.x - b.x);
+    double dist_y = (a.x - b.x) * (a.x - b.x);
+    return sqrt(dist_x + dist_y);
+}
+
+double SatGeometry::get_altitude (Coordinate a)
 {
         return (a.r - EARTH_RADIUS);
 }
 
 // Returns latitude in radians, in the range from -PI/2 to PI/2
-double SatGeometry::get_latitude(Coordinate a)
+double SatGeometry::get_latitude (Coordinate a)
 {
         return (PI/2 - a.theta);
 }
@@ -71,7 +69,7 @@ double SatGeometry::get_latitude(Coordinate a)
 // (the input coordinate corresponds to fixed coordinate system, through
 // which the Earth rotates, so we have to scale back the effects of rotation).
 // The return value ranges from -PI to PI.
-double SatGeometry::get_longitude(Coordinate coord_)
+double SatGeometry::get_longitude (Coordinate coord_)
 {
         double period = EARTH_PERIOD; // period of earth in seconds
         // adjust longitude so that it is earth-centric (i.e., account
@@ -87,3 +85,142 @@ double SatGeometry::get_longitude(Coordinate coord_)
 	else
 		return (earth_longitude);
 }       
+
+
+//---------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
+
+
+struct OrbitalSat
+{
+        double altitude;
+        double inclination;
+        double longitude;
+        double alpha;
+        uint32_t plane;
+};  
+
+class Constellation
+{
+        public:
+                Constellation(double c_alt, double c_inc, uint32_t c_nPlane, uint32_t c_nSat):
+                              alt (c_alt), inc (c_inc), nPlane (c_nPlane), nSat (c_nSat) { SetSatellite(); }
+                ~Constellation()
+                {
+                        delete[] satellite;
+                }
+                void SetSatellite();
+                void SetInit (uint32_t satNode);
+                Vector SphericalToCartesianCoordinates (double, double, double);
+                Vector SatPos (uint32_t satNode);
+                Coordinate Coord ();
+                Vector* GetPos() {return satellite;}
+                
+        protected:
+                double alt;
+                double inc;
+                uint32_t nPlane;
+                uint32_t nSat;
+                double period;
+                Coordinate init;
+                Vector* satellite = new Vector [nPlane * nSat];
+                std::vector<OrbitalSat> polarSat;
+                SatGeometry geometry;
+};
+
+void Constellation::SetSatellite ()
+{
+        double satAngle = 360.0/nSat;
+        double planeAngle = 360.0/nPlane;
+        double offsetMulti = 5.0;
+        double phaseOffset = offsetMulti/nPlane;
+        for (uint32_t a = 0; a < nPlane; a++)
+                for (uint32_t i = 0; i < nSat; i++)
+                        polarSat.push_back ({alt, inc, planeAngle*a, i*satAngle + a*phaseOffset*satAngle, a});
+}
+
+void Constellation::SetInit (uint32_t satNode)
+{
+        double satAlt = polarSat[satNode].altitude; 
+        double satLon = polarSat[satNode].longitude;
+        double satAlpha = polarSat[satNode].alpha;
+        double satIncl = polarSat[satNode].inclination;
+
+        init.r = satAlt + EARTH_RADIUS; // Altitude in km above the earth
+        if (satAlpha < 0)
+                exit(1);
+        
+        if (satAlpha >= 360)
+                init.theta = DEG_TO_RAD(satAlpha - 360);
+        else
+                init.theta = DEG_TO_RAD(satAlpha);
+
+        if (satLon < -360 || satLon > 360)
+                exit(1);
+
+        if (satLon < 0)
+                init.phi = DEG_TO_RAD(360 + satLon);
+        else
+                init.phi = DEG_TO_RAD(satLon);
+
+        if (satIncl < 0 || satIncl > 180)
+                exit(1);
+        inc = DEG_TO_RAD (satIncl);
+
+        double num = init.r * init.r * init.r;
+        period = 2 * PI * sqrt(num/MU); // seconds
+}
+
+Vector Constellation::SphericalToCartesianCoordinates (double r, double theta, double phi)
+{
+        double x = r * sin(theta) * cos(phi);
+        double y = r * sin(theta) * sin(phi);
+        double z = r * cos(theta);
+        Vector cartesian(x, y, z);
+        return cartesian;
+}
+
+Coordinate Constellation::Coord ()
+{
+	Coordinate current;
+	double partial;  // fraction of orbit period completed
+	partial = (fmod(Simulator::Now().GetSeconds(), period)/period) * 2*PI; //rad
+	double theta_cur, phi_cur, theta_new, phi_new;
+
+	// Compute current orbit-centric coordinates:
+	// theta_cur adds effects of time (orbital rotation) to init.theta
+	theta_cur = fmod(init.theta + partial, 2*PI);
+	phi_cur = init.phi;
+	// Reminder:  theta_cur and phi_cur are temporal translations of 
+	// initial parameters and are NOT true spherical coordinates.
+	//
+	// Now generate actual spherical coordinates,
+	// with 0 < theta_new < PI and 0 < phi_new < 360
+
+	//assert (inc < PI);
+
+	// asin returns value between -PI/2 and PI/2, so 
+	// theta_new guaranteed to be between 0 and PI
+	theta_new = PI/2 - asin(sin(inc) * sin(theta_cur));
+	// if theta_new is between PI/2 and 3*PI/2, must correct
+	// for return value of atan()
+	if ((theta_cur > PI/2 && theta_cur < 3*PI/2) || theta_cur == DEG_TO_RAD(270.0))
+		phi_new = atan(cos(inc) * tan(theta_cur)) + phi_cur + PI;
+	else
+		phi_new = atan(cos(inc) * tan(theta_cur)) + phi_cur;
+	phi_new = fmod(phi_new + 2*PI, 2*PI);
+	
+	current.r = init.r;
+	current.theta = theta_new;
+	current.phi = phi_new;
+	return current;
+}
+
+Vector Constellation::SatPos (uint32_t sat)
+{
+        SetInit(sat);
+        satellite[sat].x = RAD_TO_DEG(geometry.get_longitude(Coord()));
+        satellite[sat].y = RAD_TO_DEG(geometry.get_latitude(Coord()));
+        satellite[sat].z = geometry.get_altitude(Coord());
+        return satellite[sat];
+}
